650d75a9d94f613d99021de1745144361543fa6c
MarcelloSerqueira <marcello.serqueira@gmail.com>
2019-08-11 18:35:35 -0300

    Tree feature selection

--- data_utils.py
+++ data_utils.py
@@ -2,6 +2,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.metrics import precision_score, f1_score, recall_score, accuracy_score
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
 
 def csv_to_numpy_array(train_path, test_path):
 	print("Loading training data...")
@@ -28,6 +30,7 @@ def transform_data(x_train, x_test):
 	testY = np.int_(testY.reshape(-1))
 	testY = np.eye(num_classes)[testY] #one hot vector
 	#print('test data loaded successfully!', '\n')
+	trainX, testX = tree_feature_selection(trainX, trainY, testX)
 
 	return trainX, trainY, testX, testY, num_classes
 
@@ -46,3 +49,12 @@ def nn_performance_metrics(pred_model, pred_true, train_model, train_true):
   print("Test error: ", '{0:.2%}'.format(1-acc_test))
 
   return precision, recall, f1, acc_test
+
+def tree_feature_selection(trainX, trainY, testX):
+    clf = ExtraTreesClassifier(n_estimators=50)
+    clf = clf.fit(trainX, trainY)
+    model = SelectFromModel(clf, prefit=True)
+    trainX = model.transform(trainX)
+    testX = model.transform(testX)
+    print("The input data now has ", trainX.shape[1], " features!\n")
+    return trainX, testX
