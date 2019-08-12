542571176ac942709da1b1f99f66a3ec499bbf99
MarcelloSerqueira <marcello.serqueira@gmail.com>
2019-08-11 22:21:00 -0300

    Random Search to auto hyp

--- neural_network.py
+++ neural_network.py
@@ -5,7 +5,6 @@ import tensorflow as tf
 import numpy as np
 import data_utils as du
 import sys
-import math
 import warnings
 import matplotlib.pyplot as plt
 warnings.filterwarnings("ignore")
@@ -23,12 +22,12 @@ Example: python neural_network.py 100 200 300 0.001 0
 '''
 
 #Train/dev/test path whitespace separated file without header. Target in the last column (data_utils)
-train_path =  "/home/mserqueira/src/python/datasets/mnist_train.csv"
-test_path = "/home/mserqueira/src/python/datasets/mnist_val.csv"
+train_path =  "/home/mserqueira/BRKGA/src/python/datasets/mnist_train.csv"
+test_path = "/home/mserqueira/BRKGA/src/python/datasets/mnist_val.csv"
 
 epochs_no = 300 #Number of epochs
 batch_size = 32 #Batch size
-patience = 13 #Number of trials to reduce epoch loss based on the min_delta
+patience = 15 #Number of trials to reduce epoch loss based on the min_delta
 min_delta = 0.01 #min MSE reduction in each epoch
 patience_cnt = 0
 prior = float("inf")
@@ -36,29 +35,19 @@ prior = float("inf")
 trainX, trainY, predX, predY, n_classes = du.csv_to_numpy_array(train_path, test_path)
 num_x = trainX.shape[1]
 num_y = trainY.shape[1]
-learning_rate = float(sys.argv[4])
-beta = float(sys.argv[5])
 
-x = tf.placeholder(tf.float32, [None, num_x])
-y = tf.placeholder(tf.float32, [None, num_y])
-lr = tf.placeholder(tf.float32)
-
-def neural_network(LAYER1, LAYER2, LAYER3):
-   with tf.device('/device:GPU:0'):
-    units_layer1 = int(LAYER1)
-    units_layer2 = int(LAYER2)
-    units_layer3 = int(LAYER3)
+def neural_network(LAYER1, LAYER2, LAYER3, x, y):
+   with tf.device('/device:GPU:0'):    
+    W1 = tf.Variable(tf.random_normal([num_x, LAYER1]))
+    b1 = tf.Variable(tf.random_normal([LAYER1]))
     
-    W1 = tf.Variable(tf.random_normal([num_x, units_layer1]))
-    b1 = tf.Variable(tf.random_normal([units_layer1]))
+    W2 = tf.Variable(tf.random_normal([LAYER1, LAYER2]))
+    b2 = tf.Variable(tf.random_normal([LAYER2]))
     
-    W2 = tf.Variable(tf.random_normal([units_layer1, units_layer2]))
-    b2 = tf.Variable(tf.random_normal([units_layer2]))
+    W3 = tf.Variable(tf.random_normal([LAYER2, LAYER3]))
+    b3 = tf.Variable(tf.random_normal([LAYER3]))
     
-    W3 = tf.Variable(tf.random_normal([units_layer2, units_layer3]))
-    b3 = tf.Variable(tf.random_normal([units_layer3]))
-    
-    W_out = tf.Variable(tf.random_normal([units_layer3, n_classes]))
+    W_out = tf.Variable(tf.random_normal([LAYER3, n_classes]))
     b_out = tf.Variable(tf.random_normal([n_classes]))
     
     Z1 = tf.add(tf.matmul(x, W1), b1)
@@ -74,7 +63,7 @@ def neural_network(LAYER1, LAYER2, LAYER3):
 
     return Z_out, W_out
 
-def models_predictions(prediction, sess):
+def models_predictions(prediction, sess, x, y):
   pred_model =  tf.argmax(prediction, 1)
   pred_model = sess.run(pred_model, feed_dict={x:predX, y:predY})
   pred_true =  tf.argmax(y, 1)
@@ -98,6 +87,8 @@ def early_stop(epoch, epoch_loss):
     flag = False
   if patience_cnt > patience:
     flag = True
+    patience_cnt = 0
+    prior = float("inf")
   prior = epoch_loss
   return flag
 
@@ -123,8 +114,11 @@ def get_error(prediction, y, sess, labelX, labelY, x):
 
   return error
 
-def build_graph():    
-    prediction, weights = neural_network(sys.argv[1], sys.argv[2], sys.argv[3])
+def build_graph(layer1, layer2, layer3, learning_rate, beta, best_model):    
+    x = tf.placeholder(tf.float32, [None, num_x])
+    y = tf.placeholder(tf.float32, [None, num_y])
+    lr = tf.placeholder(tf.float32)
+    prediction, weights = neural_network(layer1, layer2, layer3, x, y)
     
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y)) #Ou reduce_sum
     regularizer = tf.nn.l2_loss(weights) #L2
@@ -149,26 +143,53 @@ def build_graph():
           i+=batch_size
         
         early_flag = early_stop(epoch, epoch_loss)
-        if early_flag == False:
+        if early_flag == False and (epoch%10==0):
           print('Epoch ', epoch+1, 'of ', epochs_no, '| loss: ', epoch_loss)
-        else:
+        if early_flag == True:
           print("Early stopping... ", 'Epoch', epoch+1, 'of', epochs_no, '| loss:', epoch_loss)
           break
         loss_train.append(get_error(prediction, y, sess, trainX, trainY, x))
         loss_test.append(get_error(prediction, y, sess, predX, predY, x))
 
-      pred_model, pred_true, train_model, train_true = models_predictions(prediction, sess)
+      pred_model, pred_true, train_model, train_true = models_predictions(prediction, sess, x, y)
       prec, rec, f1, acc = du.nn_performance_metrics(pred_model, pred_true, train_model, train_true)
 
+      if acc > best_model:
+        saver = tf.train.Saver()
+        saver.save(sess, 'TF_model/sess/AutoML_model.ckpt')
+        np.savetxt("TF_model/test_classification.csv", pred_model.astype(int), delimiter=",")
+        print("\nBest model saved in TF_model folder")
+
       sess.close()
-      final = datetime.datetime.now()
-      time = final-opening
-      print("\nExecution time: ", time)
+
       learning_plot(loss_train, loss_test)
       return acc
 
+def random_search(num_trials):
+    i=0
+    best_result = 0
+    while i < num_trials:
+        layers = np.random.randint(low=500, high=1000, size=3)
+        lr = float(np.around(np.random.uniform(low=0.1, high=0.0001, size=1), decimals=6))
+        rr = float(np.around(np.random.uniform(low=0, high=0.001, size=1), decimals=6))
+        print("Iteration number: ", layers, '\n')
+        max_objective = build_graph(layers[0], layers[1], layers[2], lr, rr, best_result)
+        
+        if max_objective > best_result:
+            best_result = max_objective
+            best_params = [layers[0], layers[1], layers[2], lr, rr]
+            best_trial = i
+            print("Best result Updated!\n")
+        i+=1
+        tf.reset_default_graph()
+    final = datetime.datetime.now()
+    time = final-opening
+    print("Random Search final result: ", best_result)
+    print("Random Search final params: ", best_params)
+    print("\nExecution time: ", time)
+
 def main():
-  acc = build_graph()
+  random_search(3)
   return 0
 
 if __name__ == "__main__":
