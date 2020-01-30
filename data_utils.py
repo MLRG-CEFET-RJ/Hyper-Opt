6af9d2cb851e0aa2b8dd65e202bec4ef07675e56
MarcelloSerqueira <marcello.serqueira@gmail.com>
2020-01-30 12:48:42 -0300

    Last data_utils

--- data_utils.py
+++ data_utils.py
@@ -1,4 +1,4 @@
-#08/08/2019
+#07/12/2018
 import numpy as np
 import pandas as pd
 from sklearn.metrics import precision_score, f1_score, recall_score, accuracy_score
@@ -6,14 +6,15 @@ from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.feature_selection import SelectFromModel
 
 def csv_to_numpy_array(train_path, test_path):
-	print("Loading training data...")
-	traindata = pd.read_csv(train_path, header= None, delim_whitespace=True).values
-	print("Train data loaded successfully!\n")
+	print("loading training data...")
+	#traindata = pd.read_csv(train_path, header= None, sep=',').values
+	traindata = pd.read_csv(train_path, header= None, sep=",").values
+	np.random.shuffle(traindata)
 	#traindata = np.genfromtxt(train_path, dtype=None)
 	
-	print("Loading test data...")
-	testdata = pd.read_csv(test_path, header= None, delim_whitespace=True).values
-	print("Test data loaded successfully!\n")
+	print("loading test data...")
+	#testdata = pd.read_csv(test_path, header= None, sep=',').values
+	testdata = pd.read_csv(test_path, header= None, sep=",").values
 	#testdata = np.genfromtxt(test_path, dtype=None)
 
 	return transform_data(traindata, testdata)
@@ -24,14 +25,12 @@ def transform_data(x_train, x_test):
 	
 	trainY = np.int_(trainY.reshape(-1))
 	trainY = np.eye(num_classes)[trainY] #one hot vector
-	#print('\nTraining data loaded successfully!')
+	#print('\n', 'training data loaded successfully!')
 
 	[testX, testY] = np.hsplit(x_test,[x_test.shape[1]-1])
 	testY = np.int_(testY.reshape(-1))
 	testY = np.eye(num_classes)[testY] #one hot vector
-	#print('Test data loaded successfully!', '\n')
-
-	trainX, testX = tree_feature_selection(trainX, trainY, testX)
+	#print('test data loaded successfully!', '\n')
 
 	return trainX, trainY, testX, testY, num_classes
 
@@ -44,14 +43,14 @@ def nn_performance_metrics(pred_model, pred_true, train_model, train_true):
   acc_train = accuracy_score(train_true, train_model)
   acc_test = accuracy_score(pred_true, pred_model)
 
-  print("\nTrain F1: ", '{0:.3f}'.format(f1_train))
-  print("Test F1: ", '{0:.3f}'.format(f1_test))
+  #print("\nTrain F1: ", '{0:.3f}'.format(f1_train))
+  #print("Test F1: ", '{0:.3f}'.format(f1_test))
 
-  print("\nTrain accuracy: ", '{0:.3f}'.format(acc_train))
-  print("Test accuracy: ", '{0:.3f}'.format(acc_test))
+  #print("\nTrain accuracy: ", '{0:.3f}'.format(acc_train))
+  #print("Test accuracy: ", '{0:.3f}'.format(acc_test))
 
-  print("\nTrain error: ", '{0:.2%}'.format(1-acc_train))
-  print("Test error: ", '{0:.2%}'.format(1-acc_test))
+  #print("\nTrain error: ", '{0:.2%}'.format(1-acc_train))
+  #print("Test error: ", '{0:.2%}'.format(1-acc_test))
 
   return precision, recall, f1_test, acc_test
 
